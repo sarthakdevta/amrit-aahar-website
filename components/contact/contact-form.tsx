e896262847ba3edeec34449741bd41f75ffdad7d
sarthakpersonal755-6581 <sarthakpersonal755@gmail.com>
2026-03-11 12:50:10 +0530

    update

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -89,7 +89,7 @@ export function ContactForm() {
               id="email"
               name="email"
               type="email"
-              placeholder="your.email@example.com"
+              placeholder="your.email@gmail.com"
               required
             />
           </div>
@@ -102,7 +102,7 @@ export function ContactForm() {
               id="phone"
               name="phone"
               type="tel"
-              placeholder="+91 96301 55945"
+              placeholder="+91 90095 85458"
             />
           </div>
 
